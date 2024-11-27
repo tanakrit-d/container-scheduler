7e707363069feb29591e9f3517124dafad973d59
Tanakrit-D <127579510+Tanakrit-D@users.noreply.github.com>
2024-11-28 02:00:54 +1100

    change to printf

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,20 @@
 set -euo pipefail
 
 if [ -e /var/run/docker.sock ]; then
-    echo "Info: docker socket exists. Ensure your container has the correct permissions to access it."
-    echo "If the host is MacOS you will need to implement forwarding."
-    echo "See https://forums.docker.com/t/mounting-using-var-run-docker-sock-in-a-container-not-running-as-root/34390/8"
+    printf "Info: Docker socket exists. Ensure your container has the correct permissions to access it.\n"
+    printf "If the host is MacOS, you will need to implement forwarding.\n"
+    printf "See: https://forums.docker.com/t/mounting-using-var-run-docker-sock-in-a-container-not-running-as-root/34390/8\n"
 fi
 
 if [ ! -x /usr/local/bin/supercronic ]; then
-    echo "Error: /usr/local/bin/supercronic does not exist or is not executable."
+    printf "Error: /usr/local/bin/supercronic does not exist or is not executable.\n" >&2
     exit 1
 fi
 
 CRON_FILE="${1:-/app/container-schedules.cron}"
 
 if [ ! -f "$CRON_FILE" ]; then
-    echo "Error: Cron file $CRON_FILE does not exist."
+    printf "Error: Cron file '%s' does not exist.\n" "$CRON_FILE" >&2
     exit 1
 fi
 
