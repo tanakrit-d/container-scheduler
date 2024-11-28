9bbb3e3091e646b0d09e36ab1084cccbe72e7004
Tanakrit-D <127579510+Tanakrit-D@users.noreply.github.com>
2024-11-28 19:36:32 +1100

    simplify logging

--- functions.sh
+++ functions.sh
@@ -18,14 +18,13 @@ restart_containers() {
         for container_name in $containers; do
             if curl --silent --unix-socket /var/run/docker.sock \
                 -X POST "http://localhost/containers/${container_name}/stop" > /dev/null; then
-                echo "[${timestamp}] Successfully stopped containers: ${container_name}"
 
                 # Wait a short time to ensure the container has stopped cleanly
                 sleep 1
 
                 if curl --silent --unix-socket /var/run/docker.sock \
                     -X POST "http://localhost/containers/${container_name}/start" > /dev/null; then
-                    echo "[${timestamp}] Successfully started containers: ${container_name}"
+                    echo "[${timestamp}] Successfully restarted containers: ${container_name}"
                 else
                     echo "[${timestamp}] Failed to start containers: ${container_name}" >&2
                 fi
