86f5492c3fced55423e092f9adadec9409c57c32
Tanakrit-D <127579510+Tanakrit-D@users.noreply.github.com>
2024-11-28 18:48:20 +1100

    permissions for non-root

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-set -euo pipefail
+if [ -n "$HOST_DOCKER_GID" ]; then
+    groupmod -g "$HOST_DOCKER_GID" docker
+    adduser scheduler docker
+    chown -R scheduler:docker /app /var/log /var/log/cron.log
+    printf "Info: Permissions set; GID for docker set to: $HOST_DOCKER_GID; scheduler added to group.\n"
+    printf "-----\n"
+fi
 
 if [ -e /var/run/docker.sock ]; then
     printf "Info: Docker socket exists. Ensure your container has the correct permissions to access it.\n"
     printf "If the host is MacOS, you will need to implement forwarding.\n"
     printf "See: https://forums.docker.com/t/mounting-using-var-run-docker-sock-in-a-container-not-running-as-root/34390/8\n"
+    printf "-----\n"
 fi
 
 if [ ! -x /usr/local/bin/supercronic ]; then
@@ -20,4 +27,4 @@ if [ ! -f "$CRON_FILE" ]; then
     exit 1
 fi
 
-exec /usr/local/bin/supercronic -json -quiet "$CRON_FILE"
+exec su-exec scheduler /usr/local/bin/supercronic -json -quiet "$CRON_FILE"
